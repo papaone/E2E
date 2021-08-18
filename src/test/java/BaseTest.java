338202bda527c17985642d113cf54c9b14c52e46
papaone <vitaliybugayev@gmail.com>
2021-08-18 15:21:20 +0300

    Update BaseTest.java

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -2,16 +2,15 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
-import static com.codeborne.selenide.Selenide.open;
 
 public class BaseTest {
 
-    @BeforeAll
+    @BeforeEach
     public static void openApp() {
         Configuration.browser = "chrome";
         //Configuration.baseUrl = "http://192.168.50.212:4444/wd/hub";
