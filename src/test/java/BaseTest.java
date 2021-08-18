d626aa0c859063ca213ab9306634c2d3f694d0b6
papaone <vitaliybugayev@gmail.com>
2021-08-18 21:24:35 +0300

    Add loggin tool

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -2,11 +2,14 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.aeonbits.owner.ConfigFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInstance;
 import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.slf4j.LoggerFactory;
 import utils.TestsConfig;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
@@ -15,6 +18,8 @@ import static com.codeborne.selenide.Selenide.open;
 
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
 public class BaseTest {
+
+    Logger logger = LogManager.getLogger(BaseTest.class);
 
     @BeforeEach
     public void openApp() {
@@ -31,11 +36,13 @@ public class BaseTest {
         Configuration.pageLoadStrategy = PageLoadStrategy.NONE.toString();
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(false));
         open(Configuration.baseUrl);
+        logger.info("driver start");
     }
 
     @AfterEach
     public void tearDown() {
         closeWebDriver();
+        logger.info("driver close");
     }
 }
 
