967aae990b65e41d5548618baec1917aca2e691e
papaone <vitaliybugayev@gmail.com>
2021-08-18 23:44:23 +0300

    Update MainPageSteps.java

--- src/main/java/steps/MainPageSteps.java
+++ src/main/java/steps/MainPageSteps.java
@@ -10,14 +10,14 @@ public class MainPageSteps {
 
     @Step("Navigate to 'Events' page")
     public MainPageSteps navigateToEventsPage() {
-        //mainPage.submitCookie();
+        mainPage.submitCookie();
         mainHeader.clickOnEventsTab();
         return this;
     }
 
     @Step("Navigate to 'Talks Library' page")
     public MainPageSteps navigateToTalksLibraryPage() {
-        //mainPage.submitCookie();
+        mainPage.submitCookie();
         mainHeader.clickOnVideoTab();
         return this;
     }
